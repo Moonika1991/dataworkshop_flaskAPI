cc70b24ee56bf179ff6b8c2987ac750fd6dbd8c9
Moonika1991 <monika9107@gmail.com>
2021-03-26 08:25:14 +0100

    [FIN-26]Connecting multiple sources

--- DataWorkshop/ConnectorFactory/ConnectorFactory.py
+++ DataWorkshop/ConnectorFactory/ConnectorFactory.py
@@ -8,12 +8,11 @@ from .CSVConnector import CSVConnector
 
 class ConnectorFactory(ABC):
 
-    def create_connector(self, filename) -> Connector:
+    def create_connector (self, filename) -> Connector:
         # finding path by given filename
         path = Path(__file__).resolve().parents[2]
-        for name in filename:
-            connector = json.load(open("%s\\etc\\connectors\\%s.json" % (path, name)))
-            targetclass = connector.get('type').upper() + 'Connector'
+        connector = json.load(open("%s\\etc\\connectors\\%s.json" % (path, filename)))
+        targetclass = connector.get('type').upper() + 'Connector'
 
         return  globals()[targetclass](connector)
 
--- DataWorkshop/QueryRunner.py
+++ DataWorkshop/QueryRunner.py
@@ -1,21 +1,27 @@
 from .QueryParser import QueryParser
 from .ConnectorFactory.ConnectorFactory import ConnectorFactory
+import pandas as pd
+from jsonmerge import merge
 
 
 class QueryRunner():
     def __init__(self, query):
         self.parser = QueryParser(query)
         self.source = self.parser.get_source()
+        self.func = self.parser.get_func()
+        connector_factory = ConnectorFactory()
         self.connectors = []
         for s in self.source:
-            ConnectorFactory().create_connector(s)
-        self.func = self.parser.get_func()
+            self.connectors.append(connector_factory.create_connector(s))
 
     def get_source(self):
         return self.source
 
     def run(self):
-        result = []
-        for con in self.connectors:
-            result = result.append(con.execute(self.func))
+        result = None
+        if len(self.connectors) > 1:
+            for con in self.connectors[1:]:
+                part = con.execute(self.func)
+                result = merge(result, part)
+
         return result
