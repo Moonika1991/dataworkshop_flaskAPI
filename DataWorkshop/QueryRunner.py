1cbd6e85c8d8b9b4a1c1dea51bf6616c8b779ad3
Moonika1991 <monika9107@gmail.com>
2021-03-31 12:17:51 +0200

    Sum for one column works

--- DataWorkshop/QueryRunner.py
+++ DataWorkshop/QueryRunner.py
@@ -9,19 +9,33 @@ class QueryRunner():
         self.parser = QueryParser(query)
         self.source = self.parser.get_source()
         self.func = self.parser.get_func()
-        connector_factory = ConnectorFactory()
+        self.connector_factory = ConnectorFactory()
         self.connectors = []
         for s in self.source:
-            self.connectors.append(connector_factory.create_connector(s))
+            self.connectors.append(self.connector_factory.create_connector(s))
 
     def get_source(self):
         return self.source
 
     def run(self):
         result = None
-        if len(self.connectors) > 1:
+        # dealing with complex functions
+        if list(self.func[0].keys())[0] == 'sum':
+            sum = 0
+            complex = list(self.func[0].keys())[0]
+            simple = self.func[0][complex]
+            for con in self.connectors:
+                part = con.execute(simple)
+                for dic in part:
+                    for key in dic:
+                        sum += dic[key]
+            result = sum
+
+        elif len(self.connectors) > 1:
+            result = self.connectors[0].execute(self.func)
             for con in self.connectors[1:]:
                 part = con.execute(self.func)
                 result = merge(result, part)
 
         return result
+
