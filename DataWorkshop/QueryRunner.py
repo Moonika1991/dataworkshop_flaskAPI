a32eacd8830636f3b7c4bee911b82a2a29c319df
Moonika1991 <monika9107@gmail.com>
2021-04-26 11:10:21 +0200

    Sort function created, but it isn't necessary. Just set app.config['JSON_SORT_KEYS'] to false.

--- DataWorkshop/QueryRunner.py
+++ DataWorkshop/QueryRunner.py
@@ -2,6 +2,8 @@ from .QueryParser import QueryParser
 from .ConnectorFactory.ConnectorFactory import ConnectorFactory
 import pandas as pd
 from jsonmerge import merge
+from datetime import datetime
+import collections
 
 
 class QueryRunner():
@@ -24,6 +26,11 @@ class QueryRunner():
             result = self.sum()
         elif list(self.func[0].keys())[0] == 'avg':
             result = self.avg()
+
+        # sort function isn't necessary
+        # elif list(self.func[0].keys())[0] == 'sort':
+        # result = self.sort()
+
         else:
             result = self.connectors[0].execute(self.func)
             for con in self.connectors[1:]:
@@ -59,8 +66,33 @@ class QueryRunner():
         avg = {}
 
         for key in sum.keys():
-            avg[key] = avg.get(key, sum[key]/elem_number)
+            avg[key] = avg.get(key, sum[key] / elem_number)
 
         return avg
 
+    '''
+    sort function isn't needed
+    def sort(self):
+        complex = list(self.func[0].keys())[0]
+        sort_args = self.func[0][complex]
+        # function must be a list oif dicts
+        simple = [sort_args[0]]
+        # format of keys to sort
+        dt_format = sort_args[1]
+        res = self.connectors[0].execute(simple)
+        for con in self.connectors[1:]:
+            part = con.execute(simple)
+            res = merge(res, part)
 
+        res = res[0]
+        if dt_format == 'num':
+            sort_res = collections.OrderedDict(sorted(res.items()))
+        else:
+            # list of acceptable date/time formats
+            for ch in ['a', 'A', 'B', 'w', 'm', 'p', 'y', 'd']:
+                if ch in dt_format:
+                    dt_format = dt_format.replace(ch, "%"+ch)
+            sort_res = collections.OrderedDict(sorted(res.items(), key=lambda x: datetime.strptime(x[0], dt_format)))
+
+        return [dict(sort_res)]
+    '''
